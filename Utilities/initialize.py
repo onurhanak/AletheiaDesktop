0002406a7e8a70b59cd0d0b9ae042245a1a841fc
Onurhan <onurhanak@protonmail.com>
2024-02-18 14:11:35 -0500

    revise initialization logic

--- Utilities/initialize.py
+++ Utilities/initialize.py
@@ -1,17 +1,48 @@
 import os
-from pathlib import Path
 import flet as ft
 from os.path import expanduser
 
-def initialization(page):
 
+def ask_for_library_location(page):
+    def on_folder_selected(e: ft.FilePickerResultEvent):
+        if e.path:
+            library_location = e.path
+            library_location = f"{library_location}/AletheiaLibrary"
+            os.makedirs(f"{library_location}", exist_ok=True)
+            page.client_storage.set("library", library_location)
+            dlg_modal.open = False
+            page.update()
+
+    pick_files_dialog = ft.FilePicker(on_result=on_folder_selected)
+
+    def open_file_picker(e):
+        page.add(pick_files_dialog)
+
+        pick_files_dialog.get_directory_path()
+
+    dlg_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Initialization"),
+        content=ft.Text("Please select a folder for your Aletheia library."),
+        actions=[
+            ft.TextButton("Select Folder", on_click=open_file_picker),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+    )
+
+    page.dialog = dlg_modal
+    dlg_modal.open = True
+    page.update()
+    return
+
+
+def initialization(page):
     """
     Triggered when the user runs the application
     for the first time. Set dark theme, create library, create a dummy file.
     """
 
     user_home_dir = expanduser("~")
-
 
     if not os.path.exists(f"{user_home_dir}/.aletheia"):
         page.client_storage.set("dark_theme_selected", True)
@@ -21,12 +52,9 @@ def initialization(page):
         page.theme = ft.theme.Theme(color_scheme_seed="gray")
         page.theme_mode = "DARK"
 
-        library_location = f'{user_home_dir}/AletheiaLibrary'
+        ask_for_library_location(page)
 
-        with open(f"{user_home_dir}/.aletheia", 'w') as f:
+        with open(f"{user_home_dir}/.aletheia", "w") as f:
             pass
 
-        os.makedirs(library_location, exist_ok=True)
-        page.client_storage.set('library', library_location)
-        page.client_storage.set('downloaded_books', [])
     return
